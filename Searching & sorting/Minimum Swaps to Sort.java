349380631feb8beb4a303c6e0898c871053083a5
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-08-22 12:00:53 +0530

    Update Minimum Swaps to Sort.java

--- Searching & sorting/Minimum Swaps to Sort.java
+++ Searching & sorting/Minimum Swaps to Sort.java
@@ -1,19 +1,19 @@
-Example 1:
+// Example 1:
 
-Input:
-nums = {2, 8, 5, 4}
-Output:
-1
-Explaination:
-swap 8 with 4.
-Example 2:
+// Input:
+// nums = {2, 8, 5, 4}
+// Output:
+// 1
+// Explaination:
+// swap 8 with 4.
+// Example 2:
 
-Input:
-nums = {10, 19, 6, 3, 5}
-Output:
-2
-Explaination:
-swap 10 with 3 and swap 19 with 5.
+// Input:
+// nums = {10, 19, 6, 3, 5}
+// Output:
+// 2
+// Explaination:
+// swap 10 with 3 and swap 19 with 5.
 
 
 
