efb9c61b7d5f4b3e44010e26f1acd8c39527ce75
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-08-28 06:34:49 +0530

    Update 669. Trim a Binary Search Tree.java

--- Linked List/669. Trim a Binary Search Tree.java
+++ Linked List/669. Trim a Binary Search Tree.java
@@ -12,3 +12,30 @@ class Solution {
     }
   
 }
+
+
+/////////// SECOND WAY ///////////////////
+
+
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        return removeElement(root, low, high);
+    }
+  
+    public TreeNode removeElement(TreeNode root, int low, int high){
+      if(root == null){
+        return null;
+      }
+      
+      root.left = removeElement(root.left, low, high);
+      root.right = removeElement(root.right, low, high);
+      
+      if(root.val < low){
+          return root.right;
+      }
+      else if(root.val > high){
+          return root.left;
+      }
+      return root;
+    }
+}
